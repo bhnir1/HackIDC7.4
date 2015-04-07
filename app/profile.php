107e10db4399615ad0848362860e9e3fb5219ecf
grizim <grizim@gmail.com>
2015-04-07 18:02:53 +0300

    db and right interface

---
+++ app/profile.php
@@ -0,0 +1,30 @@
+﻿<?php
+$keys = NULL;
+$values = NULL;
+echo '{';
+foreach ($_POST as $key => $value) {
+	echo "\"" . $key . "\"".' : '. "\"" . $value. "\"" . ',<br>';
+	// $keys[$i]=$key;
+	$values[$key]=$value;
+
+}
+echo '}';
+
+
+// echo '<br>';
+// if(isset($_POST[$input]))
+// 	echo 'yes';
+// else
+// 	echo 'no';
+
+
+
+?>
+
+<!--  
+select * from right where rights.$keys[i]==rights.$values[i] or  $values[i]==0
+
+
+
+-->
+
